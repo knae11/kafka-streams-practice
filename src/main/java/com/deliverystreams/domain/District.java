2a840ade3b6502dc39ce8af8a513177dfdbd2bb9
naeun <better@woowahan.com>
2022-09-06 22:41:18 +0900

    refactor: 불필요한 내용 제거

--- src/main/java/com/deliverystreams/domain/District.java
+++ src/main/java/com/deliverystreams/domain/District.java
@@ -1,18 +1,13 @@
 package com.deliverystreams.domain;
 
-import java.util.Arrays;
-import java.util.NoSuchElementException;
-
 public enum District {
     SONG_PA(111),
     GANG_NAM(112),
-    GANG_DONG(113),
-    @Deprecated
-    GAND_DONG(114);
+    GANG_DONG(113);
 
     private int code;
 
     District(int code) {
         this.code = code;
     }
-}
+    }
